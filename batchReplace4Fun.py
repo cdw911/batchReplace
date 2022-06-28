598e6765d3780935c818ef531cffdee32eff62a8
cdw911 <37199433+cdw911@users.noreply.github.com>
2022-06-28 23:21:10 +0800

    Update batchReplace4Fun.py

--- batchReplace4Fun.py
+++ batchReplace4Fun.py
@@ -14,19 +14,7 @@ def batchReplace(func):
         res_dict = dict(zip(tag_list, replace_list))
         print(res_dict)
         return func()
-    # tmp = u'tmp.txt'
-    # res = r'./res.txt'
-    # with open(tmp, encoding="utf-8") as f:
-    #     _res = f.read()
-    #     print("目标文件内容：", _res)
-    #     for i in res_dict:
-    #     #     _res = _res.replace(i, res_dict[i])
-    #     # print(_res)
-    #     #查找字典内键值的所有位置
-    #         print(_res.find(i))
 
-    #     # with open(res, 'a', encoding="utf-8") as tag_f:
-    #     #     tag_f.write('\n'+_res)
 
 @batchReplace
 def func(tag_list:list,replace_list:list,text:str):#记录单词的位置,对文本进行替换
@@ -50,7 +38,7 @@ def func(tag_list:list,replace_list:list,text:str):#记录单词的位置,对文
         
 # test 
 if __name__ == "__main__":
-    # batchReplace()
+
     tag_list = ['1','2','3']
     replace_list = ['啊啊','嗯嗯','哈~~']
     text = '1 2 31 2 31 2 31 2 31 2 31 2 31 2 3 帕秋莉go'
